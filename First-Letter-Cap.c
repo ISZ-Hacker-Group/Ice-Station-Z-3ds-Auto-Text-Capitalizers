af108a5ad72bddf4a7f851b03e35faf576a5f03d
KingCraXy <cobergage@gmail.com>
2022-05-03 00:15:15 -0500

    Add files via upload

---
+++ First-Letter-Cap.c
@@ -0,0 +1,110 @@
+﻿void	First_Letter_Cap(void)
+{
+	if (READU16(offset + 0x0429F44) == 0x00000061)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000041);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000062)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000042);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000063)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000043);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000064)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000044);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000065)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000045);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000066)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000046);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000067)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000047);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000068)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000048);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000069)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000049);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006A)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004A);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006B)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004B);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006C)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004C);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006D)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004D);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006E)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004E);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000006F)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000004F);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000070)
+	{
+		WRITEU32(offset + 0x0429F44, 0x00000050);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000071)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000051);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000072)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000052);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000073)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000053);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000074)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000054);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000075)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000055);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000076)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000056);
+	}
+	if (READU16(offset + 0x0429F44) == 0x40000077)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000057);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000078)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000058);
+	}
+	if (READU16(offset + 0x0429F44) == 0x00000079)
+	{
+		WRITEU8(offset + 0x0429F44, 0x00000059);
+	}
+	if (READU16(offset + 0x0429F44) == 0x0000007A)
+	{
+		WRITEU8(offset + 0x0429F44, 0x0000005A);
+	}
+	offset = 0;
+	data = 0;
+}
+
